dde1c5ad5744110d5b6b0746e24e12a918f10878
Gerald <gerald.programming.chong@gmail.com>
2022-07-26 00:06:56 +0800

    homepage

--- src/pages/homepage/Slider.js
+++ src/pages/homepage/Slider.js
@@ -1,17 +1,27 @@
-import React, {setState} from "react";
+import React from "react";
+import { Link } from "react-router-dom";
 
-const slider = [
-    "/images/slide1.jpg",
-    "/images/slide2.jpg",
-    "/images/slide3.jpg",
-];
 
 function Slider() {
     return (
-        <div>
-            <img src={slider[0]} alt="" />
-            <img src={slider[1]} alt="" />
-            <img src={slider[2]} alt="" />
+        <div className="bg-zinc-800 h-screen w-screen flex justify-center overflow-y-hidden overflow-x-hidden">
+            <div className="translate-y-1/2 text-center">
+                <h1 className="text-9xl font-bold text-white drop-shadow-lg uppercase">
+                    GETLAW
+                </h1>
+                <p className="text-5xl font-bold text-white drop-shadow-lg mt-20">
+                    Matching you with the best lawyers
+                </p>
+                <div className="space-x-96 mt-64">
+                    <Link to="/login" className="text-white text-4xl font-bold drop-shadow-xl">
+                        Login
+                    </Link>
+                    <Link to="/register" className="text-white text-4xl font-bold drop-shadow-xl">
+                        Register
+                    </Link>
+                </div>
+            </div>
+            
         </div>
     );
 }
